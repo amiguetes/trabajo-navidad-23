fb31107517957dc97074fc68128766cd095d7fda
Arturo Candela <a.candelamolto@edu.gva.es>
2024-01-11 08:13:39 +0100

    Esqueleto inicial y primeras pruebas con el código del ref #1

--- Ejercicio1/src/main/java/org/example/App.java
+++ Ejercicio1/src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.io.*;
 /**
  * Hello world!
  *
@@ -8,6 +11,50 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+        if (0 >= args.length) {
+            System.out.println("No se ha especificado ningún argumento");
+            System.exit(1);
+        }
+
+        List<String> argumentos = Arrays.asList(args);
+
+        ProcessBuilder pb = new ProcessBuilder(argumentos);
+
+        try {
+            Process hijo = pb.start();
+            
+            if (!hijo.waitFor(2, TimeUnit.SECONDS)){
+                System.out.println("El comando ha tardado más de 2 segundos en ejecutarse");
+                hijo.destroy();
+                System.exit(1);
+            }
+
+            if (hijo.exitValue() == 0) {
+
+                Scanner sc = new Scanner(hijo.getInputStream());
+                PrintWriter pw = new PrintWriter(new FileWriter("output.txt"), true);
+
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    pw.println(line);
+                    
+                }
+                sc.close();
+                pw.close();
+            } else {
+                System.out.println("El comando ha devuelto un código de error");
+                System.exit(1);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error al ejecutar el comando" + e.getLocalizedMessage());
+            System.exit(1);
+        }
+
+        
+
     }
+
+
 }
