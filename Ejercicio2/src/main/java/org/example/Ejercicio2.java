d081fd7f5c09b91929d34f01b6e9823a3794e565
Arturo Candela <acandela@cipfpbatoi.es>
2024-01-12 11:46:50 +0100

    Primera versión funcional, ahora falta generar el jar y probar refs #3

--- Ejercicio2/src/main/java/org/example/Ejercicio2.java
+++ Ejercicio2/src/main/java/org/example/Ejercicio2.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Ejercicio2
@@ -11,11 +13,10 @@ public class Ejercicio2
     private static final String END_MESSAGE = "Encantado de haber jugado contigo, un saludo.";
     private static final String DEFAULT_REVERSE_JAR_LOCATION = "Reverse/target/Reverse-1.0-SNAPSHOT-jar-with-dependencies.jar";
     private static final String JAVA_JAR_COMMAND = "java -jar ";
+    private static final String REVERSE_OUTPUT_FILE_NAME = "reverses.txt";
 
     public static void main( String[] args )
     {
-        System.out.println(WELCOME_MESSAGE);
-
         String reverse_jar_location = args.length > 0 ? args[0] : DEFAULT_REVERSE_JAR_LOCATION;
 
         String process_path = JAVA_JAR_COMMAND + reverse_jar_location;
@@ -25,15 +26,41 @@ public class Ejercicio2
         try {
             Process reverse = pb.start();
 
-            try (Scanner entradaUsuario = new Scanner(System.in)){
+            try (Scanner entradaUsuario = new Scanner(System.in);
+                PrintWriter entradaReverse = new PrintWriter(reverse.getOutputStream(),true);
+                Scanner salidaEstandarReverse = new Scanner(reverse.getInputStream());
+                PrintWriter escrituraFichero = new PrintWriter(new FileWriter(REVERSE_OUTPUT_FILE_NAME),true)
+                ){
+                
+                    System.out.println(WELCOME_MESSAGE);
+
+                    String lineaUsuario = "";
+                    String lineaReverse = "";
+
+                    while (!(lineaUsuario = entradaUsuario.nextLine()).equalsIgnoreCase(STOP_MESSAGE)) {
+                        entradaReverse.println(lineaUsuario);
+                        lineaReverse = salidaEstandarReverse.nextLine();
+
+                        System.out.println(GAME_STRING_GENERATED_MESSAGE + lineaReverse);
+
+                        escrituraFichero.println(lineaUsuario);
+                        escrituraFichero.println(lineaUsuario);
+
+                    }
+
+                    System.out.println(END_MESSAGE);
+                    reverse.destroy();
+                    reverse.waitFor();
+
 
             }
-
 
         } catch (IOException e) {
             System.err.println("Error al ejecutar el proceso de Reverse en la ruta: "+reverse_jar_location);
             System.err.println(e.getLocalizedMessage());
             System.exit(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
