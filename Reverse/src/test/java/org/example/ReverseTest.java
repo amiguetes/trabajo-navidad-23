eba749312af54315529860e299ad01a1b91dd9e0
Arturo Candela <acandela@cipfpbatoi.es>
2024-01-12 10:44:11 +0100

    Después de revisar el proyecto, ya funciona todo sin problemas, construye y prueba bien refs #3

--- Reverse/src/test/java/org/example/ReverseTest.java
+++ Reverse/src/test/java/org/example/ReverseTest.java
@@ -7,7 +7,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-@Disabled("Hasta que encuentre como hacerlo funcionar en maven")
+
 public class ReverseTest {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -23,8 +23,8 @@ public class ReverseTest {
         System.setOut(originalOut);
     }
 
- 
-    private void testReverse() {
+    @Test
+    public void testReverse() {
         final String LINE_SEPARATOR = System.lineSeparator();
         String hello = "hello";
         String world = "world";
