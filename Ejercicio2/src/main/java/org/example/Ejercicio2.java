ac148289350d29adec8c43339d5adefeebbebce1
Arturo Candela <acandela@cipfpbatoi.es>
2024-01-12 11:19:35 +0100

    Primera versión funcional ejecutando el jar y con inheritIO refs #3

--- Ejercicio2/src/main/java/org/example/Ejercicio2.java
+++ Ejercicio2/src/main/java/org/example/Ejercicio2.java
@@ -1,9 +1,36 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Ejercicio2
 {
+    private static final String STOP_MESSAGE = "stop";
+    private static final String WELCOME_MESSAGE = "A partir de ahora todas las cadenas que me introduzcas y pulses enter yo les daré la vuelta, cuando termines, escribe la cadena: "+STOP_MESSAGE;
+    private static final String GAME_STRING_GENERATED_MESSAGE = "La cadena dada la vuelta es: ";
+    private static final String END_MESSAGE = "Encantado de haber jugado contigo, un saludo.";
+    private static final String DEFAULT_REVERSE_JAR_LOCATION = "Reverse/target/Reverse-1.0-SNAPSHOT-jar-with-dependencies.jar";
+    private static final String JAVA_JAR_COMMAND = "java -jar ";
+
     public static void main( String[] args )
     {
-        System.out.println( "Ejercicio 2" );
+        System.out.println(WELCOME_MESSAGE);
+
+        String reverse_jar_location = args.length > 0 ? args[0] : DEFAULT_REVERSE_JAR_LOCATION;
+
+        String process_path = JAVA_JAR_COMMAND + reverse_jar_location;
+
+        ProcessBuilder pb = new ProcessBuilder(process_path.split(" "));
+
+        pb.inheritIO();
+
+        try {
+            pb.start().waitFor();
+        } catch (InterruptedException | IOException e) {
+            
+            e.printStackTrace();
+        }
+
+
     }
 }
